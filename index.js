cb4758a32fdfe8e6e2cd6ce676c55de73a01ea93
ViPER511 <55343654+ViPER511@users.noreply.github.com>
2019-09-15 09:05:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -353,4 +353,4 @@ client.user.setGame(`1play | Last Music`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NTc1NzUyMDMzNjU5OTEyMjAz.XX3wnQ.tFZCoJb5MyEDjAP6VetCyOFeNaw);
